20ec1b5c6308be089b5fa2f3db44d08c176d8a6c
peter <97747863+wcorn@users.noreply.github.com>
2023-12-15 23:36:44 +0900

    [feat] swagger 개발

--- src/main/java/com/fubao/project/global/config/OpenApiConfig.java
+++ src/main/java/com/fubao/project/global/config/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,15 +14,22 @@ import java.util.Collections;
 
 @Configuration
 public class OpenApiConfig {
-
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openAPI(@Value("${openapi.service.title}") String serviceTitle,
+                           @Value("${openapi.service.version}") String serviceVersion,
+                           @Value("${openapi.service.url}") String url) {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER).name("Authorization");
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("jwt");
         Info info = new Info()
-                .title("fubao")
-                .version("1.0")
+                .title(serviceTitle)
+                .version(serviceVersion)
                 .description("base project");
         return new OpenAPI()
-                .addServersItem(new Server().url("/"))
+                .addServersItem(new Server().url(url))
+                .components(new Components().addSecuritySchemes("bearerAuth",securityScheme))
+                .security(Collections.singletonList(securityRequirement))
                 .info(info);
     }
 }
