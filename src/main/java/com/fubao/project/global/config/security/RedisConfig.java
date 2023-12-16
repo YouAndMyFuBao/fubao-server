828e2590ba3c28358ddce62d7e8f78d8d59c5405
peter <97747863+wcorn@users.noreply.github.com>
2023-12-17 02:12:20 +0900

    Feat: redis
    레디스 세팅 및 yml 파일 연결
    
    #10

---
+++ src/main/java/com/fubao/project/global/config/security/RedisConfig.java
@@ -0,0 +1,2 @@
+package com.fubao.project.global.config.security;public class RedisConfig {
+}
---
+++ src/main/java/com/fubao/project/global/util/RedisUtil.java
@@ -0,0 +1,39 @@
+package gcu.mp.redis;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class RedisUtil {
+    private final StringRedisTemplate redisTemplate;
+
+    public String getData(String key) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
+    public boolean existData(String key) {
+        return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+    }
+
+    public void setDataExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = redisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+
+
+    public void deleteData(String key) {
+        redisTemplate.delete(key);
+    }
+}
