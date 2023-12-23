db900742d0aa89e91cea44a2b4e65a0b4c8fd028
peter <97747863+wcorn@users.noreply.github.com>
2023-12-23 16:04:59 +0900

    Fix: security 수정
    cors 특정 url로 수정, cort, permi urls yml으로 관리,filterchain에 들어갈 내용들 lambda 형식으로 변경
    
    #36

--- src/main/java/com/fubao/project/global/config/security/SecurityConfig.java
+++ src/main/java/com/fubao/project/global/config/security/SecurityConfig.java
@@ -5,12 +5,14 @@ import com.fubao.project.global.config.security.jwt.JwtAuthenticationCheckFilter
 import com.fubao.project.global.config.security.jwt.JwtAuthenticationEntryPoint;
 import com.fubao.project.global.config.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.authentication.AuthenticationManagerFactoryBean;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,46 +23,35 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Collections;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Value("${security.permitted-urls}")
+    private final List<String> PERMITTED_URLS;
 
-    private static final String[] PERMITTED_URLS = {
-            "/api/swagger-ui/**", //swagger
-            "/api/swagger-resources/**", //swagger
-            "/api/v3/api-docs/**",  //swagger
-            "/api/auth/**",
-            "/api/test/**",
-    };
-
-    @Bean
-    public static BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
+    @Value("${security.cors-urls}")
+    private final List<String> CORS_URLS;
     @Bean
     public SecurityFilterChain filterChain(
             HttpSecurity http, JwtAuthenticationCheckFilter jwtAuthenticationCheckFilter,
             JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) throws Exception {
         http
-                .cors()//CORS 허용 정책(Front , Back 사이에 도메인이 달라지는 경우)
-                .and()
-                .csrf().disable() // //서버에 인증정보를 저장하지 않기 때문에 굳이 불필요한 csrf 코드들을 작성할 필요가 없다.
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //스프링시큐리티가 생성하지도않고 기존것을 사용하지도 않음 ->JWT 같은토큰방식을 쓸때 사용하는 설정
-                .and()
-                .formLogin().disable()  // 서버에서  View를 배포하지 안으므로 disable
-                .httpBasic().disable() // JWT 인증 방식을 사용하기에 httpBasic을 이용한 인증방식 사용X
+                .cors(httpSecurityCorsConfigurer -> corsConfigurationSource())//CORS 허용 정책(Front , Back 사이에 도메인이 달라지는 경우)
+                .csrf(AbstractHttpConfigurer::disable) //서버에 인증정보를 저장하지 않기 때문에 굳이 불필요한 csrf 코드들을 작성할 필요가 없다.
+                .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //스프링시큐리티가 생성하지도않고 기존것을 사용하지도 않음 ->JWT 같은토큰방식을 쓸때 사용하는 설정
+                .formLogin(AbstractHttpConfigurer::disable)// 서버에서  View를 배포하지 안으므로 disable
+                .httpBasic(AbstractHttpConfigurer::disable) // JWT 인증 방식을 사용하기에 httpBasic을 이용한 인증방식 사용X
                 .addFilterAfter(jwtAuthenticationCheckFilter, UsernamePasswordAuthenticationFilter.class)
                 .exceptionHandling(config -> config
-                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint) //인증 중 예외 발생 시 jwtAuthenticationEntryPoint 호출
                 )
                 .authorizeHttpRequests(request -> request
-                        .requestMatchers(PERMITTED_URLS).permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers(PERMITTED_URLS.toArray(new String[0])).permitAll() // 해당 url의 경우 검사하지 않음
+                        .anyRequest().authenticated() // 모든 request는 검증
                 );
         return http.build();
     }
@@ -69,10 +60,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(CORS_URLS);
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
@@ -94,6 +85,7 @@ public class SecurityConfig {
     public JwtAuthenticationCheckFilter jwtAuthenticationCheckFilter(JwtTokenProvider jwtTokenProvider) {
         return new JwtAuthenticationCheckFilter(jwtTokenProvider);
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
