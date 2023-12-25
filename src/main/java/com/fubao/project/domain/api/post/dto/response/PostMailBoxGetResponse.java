9f135623c348025f6c8ffa0fa630de23e260f22f
peter <97747863+wcorn@users.noreply.github.com>
2023-12-25 20:21:42 +0900

    Fix: post list 조회 response postId 추가
    우체통 조회, 내가 쓴 게시물 조회 수정
    
    #42

--- src/main/java/com/fubao/project/domain/api/post/dto/response/PostMailBoxGetResponse.java
+++ src/main/java/com/fubao/project/domain/api/post/dto/response/PostMailBoxGetResponse.java
@@ -1,10 +1,12 @@
 package com.fubao.project.domain.api.post.dto.response;
 
 import com.fubao.project.domain.entity.Post;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,12 +16,19 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 public class PostMailBoxGetResponse {
-    String imageUrl;
-    String content;
-    LocalDateTime time;
+    @Schema(description = "post id", example = "1")
+    private Long postId;
+    @Schema(description = "게시글 이미지 주소", example = "https://d3hubo81q4ghxt.cloudfront.net/local/post/3cebc0ed-f1dd-4fab-9351-96efa8b912fa.jpg")
+    private String imageUrl;
+    @Schema(description = "게시글 내용", example = "hiii222")
+    private String content;
+    @Schema(description = "게시글 생성일", example = "2023-12-20T02:08:50")
+    private LocalDateTime date;
+
     public PostMailBoxGetResponse(Post post) {
+        this.postId = post.getId();
         this.imageUrl = post.getImageUrl();
         this.content = post.getContent();
-        this.time = post.getCreatedAt();
+        this.date = post.getCreatedAt();
     }
 }
