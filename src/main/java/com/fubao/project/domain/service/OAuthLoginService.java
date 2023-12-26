719261aec6ebb6805f5549b29585efc5450b6966
peter <97747863+wcorn@users.noreply.github.com>
2023-12-27 02:11:52 +0900

    Feat: 로그아웃 구현
    refresh token redis에서 삭제
    
    #50

--- src/main/java/com/fubao/project/domain/service/OAuthLoginService.java
+++ src/main/java/com/fubao/project/domain/service/OAuthLoginService.java
@@ -1,5 +1,6 @@
 package com.fubao.project.domain.service;
 
+import com.fubao.project.domain.api.auth.dto.request.LogoutRequest;
 import com.fubao.project.domain.api.auth.dto.request.TokenRegenerateRequest;
 import com.fubao.project.domain.api.auth.dto.response.AuthTokens;
 import com.fubao.project.global.common.oauth.OAuthLoginParams;
@@ -8,4 +9,6 @@ public interface OAuthLoginService {
     AuthTokens login(OAuthLoginParams params);
 
     AuthTokens tokenRegenerate(TokenRegenerateRequest tokenRegenerateRequest);
+
+    void logout(LogoutRequest logoutRequest);
 }
--- src/main/java/com/fubao/project/global/common/api/CustomResponseCode.java
+++ src/main/java/com/fubao/project/global/common/api/CustomResponseCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CustomResponseCode {
-    MEMBER_SIGNUP("회원가입을 성공 하였습니다.");
+    MEMBER_SIGNUP("회원가입을 성공 하였습니다."),
+    MEMBER_LOGOUT("로그아웃하였습니다.");
     private final String message;
 }
